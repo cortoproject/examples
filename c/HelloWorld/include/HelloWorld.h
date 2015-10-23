dfa8f204d8be880985a09f523585f7f0eb8fc14e
Sander Mertens <sander.mertens8@gmail.com>
2015-10-22 22:03:26 -0400

    Add HelloWorld example for C

---
+++ c/HelloWorld/include/HelloWorld.h
@@ -0,0 +1,22 @@
+/* include/HelloWorld.h
+ *
+ * This file is generated. Do not modify.
+ */
+
+#ifndef HelloWorld_H
+#define HelloWorld_H
+
+#include "corto.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* $header() */
+/* You can put your own definitions here! */
+/* $end */
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ c/HelloWorld/src/HelloWorld.c
@@ -0,0 +1,8 @@
+#include "HelloWorld.h"
+
+int HelloWorldMain(int argc, char *argv[]) {
+
+    printf("Hello Corto!\n");
+
+    return 0;
+}
