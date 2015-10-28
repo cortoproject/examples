c84151454d73d8a95406bf8001e62d282bbd98a4
Sander Mertens <sander.mertens8@gmail.com>
2015-10-28 18:32:15 -0400

    Add dynamic type example

---
+++ c/DynamicType/include/DynamicType.h
@@ -0,0 +1,22 @@
+/* include/DynamicType.h
+ *
+ * This file is generated. Do not modify.
+ */
+
+#ifndef DynamicType_H
+#define DynamicType_H
+
+#include "corto.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* $header() */
+/* You can put your own definitions here! */
+/* $end */
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ c/DynamicType/src/DynamicType.c
@@ -0,0 +1,23 @@
+#include "DynamicType.h"
+
+int DynamicTypeMain(int argc, char *argv[]) {
+
+    /* Create a new struct type */
+    corto_struct Point = corto_structDeclareChild(NULL, "Point");
+    corto_memberCreateChild(Point, "x", corto_int32_o, 0);
+    corto_memberCreateChild(Point, "y", corto_int32_o, 0);
+    corto_structDefine(Point, NULL, 0);
+
+    /* Create an object with the struct type */
+    corto_object p = corto_createChild(NULL, "p", Point);
+
+    /* Dynamically assign a value to it */
+    corto_fromStr(&p, "{10, 20}");
+
+    /* Print the value to the console */
+    corto_string value = corto_str(p, 0);
+    printf("value of %s is %s\n", corto_nameof(p), value);
+    corto_dealloc(value);
+
+    return 0;
+}
