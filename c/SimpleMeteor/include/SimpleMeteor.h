34bb4103f55d3ab9ec7e6b80b741ace73ce53df9
Sander Mertens <sander.mertens8@gmail.com>
2015-12-28 23:38:50 -0500

    Add SimpleMeteor example

---
+++ c/SimpleMeteor/include/SimpleMeteor.h
@@ -0,0 +1,23 @@
+/* include/SimpleMeteor.h
+ *
+ * This file is generated. Do not modify.
+ */
+
+#ifndef SimpleMeteor_H
+#define SimpleMeteor_H
+
+#include "corto.h"
+#include "corto/web/web.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* $header() */
+/* You can put your own definitions here! */
+/* $end */
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
