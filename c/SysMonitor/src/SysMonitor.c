106cd15eb6bc1eb1d8ff777cb4a9353744f99064
Sander Mertens <sander.mertens8@gmail.com>
2016-03-13 00:45:21 -0500

    Update SysMonitor example

--- c/SysMonitor/src/SysMonitor.c
+++ c/SysMonitor/src/SysMonitor.c
@@ -3,26 +3,24 @@
 int SysMonitorMain(int argc, char *argv[]) {
 
     /* Create DDP server on port 8000 */
-    server_DDPCreateChild(root_o, "ddp", 8000);
+    server_DDPCreateChild_auto(root_o, ddp, 8000);
 
     /* Create Stats collection */
-    corto_object Stats_o = corto_voidCreateChild(root_o, "Stats");
+    corto_voidCreateChild_auto(root_o, Stats);
 
     /* Create one system monitor for the current system */
-    // sys_Monitor mon_o = sys_MonitorCreateChild(Stats_o, "mon");
+    sys_MonitorCreateChild_auto(Stats, mon);
 
     /* Resolve static cpu information once */
-    // sys_Monitor_refresh(mon_o, Sys_CpuInfo);
-    corto_int32 *i = corto_int32CreateChild(Stats_o, "mon", 10);
+    sys_Monitor_refresh(mon, Sys_CpuInfo);
 
     /* Infinite update loop */
     while (1) {
         /* The refresh method obtains new data and updates the object */
-        // sys_Monitor_refresh(mon_o, Sys_Cpu | Sys_Mem);
-        corto_int32Update(i, 12);
+        sys_Monitor_refresh(mon, Sys_Cpu | Sys_Mem);
 
         /* Refresh every second */
-        corto_sleep(0, 0);
+        corto_sleep(1, 0);
     }
 
     return 0;
