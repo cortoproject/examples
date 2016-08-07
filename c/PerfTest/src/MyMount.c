419ff7723e1245357195852f85a653b7ee013fd7
Sander Mertens <sander.mertens8@gmail.com>
2016-08-06 18:29:31 -0700

    Add PerfTest example

---
+++ c/PerfTest/src/MyMount.c
@@ -0,0 +1,20 @@
+/* $CORTO_GENERATED
+ *
+ * MyMount.c
+ *
+ * Only code written between the begin and end tags will be preserved
+ * when the file is regenerated.
+ */
+
+#include <PerfTest/PerfTest.h>
+
+corto_void _PerfTest_MyMount_onUpdate(
+    PerfTest_MyMount this,
+    corto_object observable)
+{
+/* $begin(PerfTest/MyMount/onUpdate) */
+
+    printf("update: %s\n", corto_idof(observable));
+
+/* $end */
+}
