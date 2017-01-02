569d3964228f03ca166133bc64362c8d03607d9b
Sander Mertens <sander.mertens8@gmail.com>
2017-01-01 23:59:28 -0800

    Add sys_rest example

---
+++ c/web/sys_rest/include/sys_rest.h
@@ -0,0 +1,27 @@
+/* sys_rest.h
+ *
+ * This file is generated. Do not modify.
+ */
+
+#ifndef _SYS_REST_H
+#define _SYS_REST_H
+
+#include <corto/corto.h>
+
+#include <corto/web/server/server.h>
+#include <corto/sys/sys.h>
+#include <corto/core/c/c.h>
+#include <corto/lang/c/c.h>
+/* $header() */
+/* You can put your own definitions here! */
+/* $end */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ c/web/sys_rest/src/sys_rest.c
@@ -0,0 +1,16 @@
+#include "sys_rest.h"
+
+int sys_restMain(int argc, char *argv[]) {
+
+    sys_Monitor mon = sys_MonitorCreateChild(root_o, "mon");
+
+    server_RESTCreate(8000, "api", "");
+    server_FilesCreate(8000, "", ".");
+
+    while (1) {
+        sys_Monitor_refresh(mon, Sys_Cpu | Sys_Mem);
+        corto_sleep(1, 0);
+    }
+
+    return 0;
+}
