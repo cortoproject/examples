8c09f38de442c67488fe4f7dc9c116a9bb12f836
Alex Hendren <alex@oa-sys.io>
2017-10-07 01:48:06 -0500

    Initialize http router package

---
+++ c/http/router/include/router.h
@@ -0,0 +1,29 @@
+/* router.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef ROUTER_H
+#define ROUTER_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <router/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <router/_type.h>
+#include <router/_interface.h>
+#include <router/_load.h>
+#include <router/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
---
+++ c/http/router/src/router.c
@@ -0,0 +1,11 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <router/router.h>
+
+int routerMain(int argc, char *argv[]) {
+
+    /* Insert implementation */
+    
+    return 0;
+}
+
